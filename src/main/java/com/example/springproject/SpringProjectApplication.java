9bc67eed35ee04725ac9745661616038a4b33cde
Nguyen Duy Quyen <nguyenduyquyen0017@gmail.com>
2024-04-05 17:05:59 +0700

    SpringBoot Web and Spring profile

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -1,13 +1,27 @@
 package com.example.springproject;
 
+import com.example.springproject.profiles.DatabaseConnectionProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class SpringProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectApplication.class, args);
+
+//        SpringApplication application = new SpringApplication(SpringApplication.class);
+//        ConfigurableEnvironment configurableEnvironment = new StandardEnvironment();
+//
+//        configurableEnvironment.setActiveProfiles("dev");
+//        application.setEnvironment(configurableEnvironment);
+//
+//        ConfigurableApplicationContext context = application.run(args);
+//        DatabaseConnectionProvider databaseConnectionProvider = context.getBean(DatabaseConnectionProvider.class);
+//        System.out.println(databaseConnectionProvider);
     }
 
 }
--- src/main/java/com/example/springproject/controller/MainController.java
+++ src/main/java/com/example/springproject/controller/MainController.java
@@ -4,6 +4,7 @@ import com.example.springproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,5 @@ public class MainController {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "Duy Quyền") String name) {
         return String.format("Hello %s", name);
     }
+
 }
