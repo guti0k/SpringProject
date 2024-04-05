722553faabc2da6473ebced53632f3415362cf81
Nguyen Duy Quyen <nguyenduyquyen0017@gmail.com>
2024-04-05 16:19:44 +0700

    Annotation và SpringBoot Configuration

--- src/main/java/com/example/springproject/controller/MainController.java
+++ src/main/java/com/example/springproject/controller/MainController.java
@@ -1,11 +1,20 @@
 package com.example.springproject.controller;
 
+import com.example.springproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${server.port}")
+    private Integer port;
 
     @PostMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "Duy Quyền") String name) {
---
+++ src/main/java/com/example/springproject/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.springproject.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void addUser() {
+        System.out.println("Thêm user thành công");
+    }
+}
